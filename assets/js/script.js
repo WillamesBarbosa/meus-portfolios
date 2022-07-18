9d17d7432f9e8f2d7b4299e3a34c8b0068815178
Willames da S. Barbosa <104446605+WillamesBarbosa@users.noreply.github.com>
2022-07-18 10:56:26 -0300

    Primeiro commit (versão incompleta)
    Até aqui temos apenas o header e parte do corpo do site.

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+//Função que muda tem light mode para dark mode e vice versa
+function mudarTema(){
+    //Aqui pego o body e a classe dele para poder comparar
+    let layout = document.getElementById('tema');
+    let temaLayout = layout.getAttribute('class');
+// Aqui faço a comparação e mudo no if para dark mode e no else para light mode
+    if(temaLayout == "body"){
+        layout.classList.add('dark')
+
+        //Mudando o ícone para Dark
+        let icone = document.getElementById('icone');
+        icone.classList.remove('mdi-weather-night');
+        icone.classList.add('mdi-white-balance-sunny');
+
+        //Mudando o header para Dark
+        let header = document.getElementById('header');
+        header.classList.add('dark');
+    }else{
+        //Removendo modo Dark do body
+        layout.classList.remove('dark')
+
+        //Alternando o botão do Dark Mode
+        icone.classList.remove('mdi-white-balance-sunny');
+        icone.classList.add('mdi-weather-night');
+
+        //Removendo modo Dark do header
+        header.classList.remove('dark');
+    }
+
+}
+
+//Seção do Menu responsivo
+
+function abrirMenu(){
+    let content = document.getElementById('mainID')
+    //Pegando o menu e a classe para comparar
+    let menu = document.getElementById('menu');
+    let classMenu = menu.getAttribute('class');
+    //Comparando a classe com a classe atual
+    if(classMenu == 'menu'){
+        menu.classList.add('clicado');
+        content.classList.add('hide')
+    }else{
+        menu.classList.remove('clicado');
+        content.classList.remove('hide')
+
+    }
+
+    menu.addEventListener('click',function(event){
+        if(classMenu == 'menu clicado'){
+            menu.classList.remove('clicado');
+            content.classList.remove('hide')
+
+        }
+    })
+
+}
+
+function fecharMenu(){
+    let menu = document.getElementById('menu');
+    let classMenu = menu.getAttribute('class');
+    if(classMenu == 'menu clicado'){
+        menu.classList.remove('clicado');
+
+    }
+}
